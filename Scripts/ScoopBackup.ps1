52a17e7d996c7b5527192a6c9834ac56efefd95b
wallstop <wallstop@wallstopstudios.com>
2024-10-19 18:23:29 -0700

    Backup for 2024/10/19 06:23:29

--- Scripts/ScoopBackup.ps1
+++ Scripts/ScoopBackup.ps1
@@ -1,4 +1,6 @@
 $root_directory = git rev-parse --show-toplevel
 Push-Location "$root_directory"
 scoop export -c > Config/scoopfile.json
+$content = Get-Content -Path Config/scoopfile.json -Encoding Unicode
+$content | Out-File -FilePath COnfig/scoopfile.json -Encoding utf8
 Pop-Location
