706a24cc0a28bd2eed0052db14941e8e7d090f71
wallstop <wallstop@wallstopstudios.com>
2024-10-19 18:18:27 -0700

    Backup for 2024/10/19 06:18:27

--- Scripts/Backup.ps1
+++ Scripts/Backup.ps1
@@ -2,9 +2,10 @@ $root_directory = git rev-parse --show-toplevel
 Push-Location "$root_directory/Scripts/"
 .\ScoopUpdate.ps1
 .\ScoopBackup.ps1
-git add --all
 $date = Get-Date
 $date_string = "{0:yyyy/MM/dd hh:mm:ss}" -f $date
+git add --all
 git commit -m "Backup for $date_string"
+git pull origin main
 git push origin main
 Pop-Location
