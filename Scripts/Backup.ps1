cad67ffdacda5ee6c7c09614a4aaef032acc661d
wallstop <wallstop@wallstopstudios.com>
2024-10-19 18:10:17 -0700

    Backup for 2024/10/19 06:10:17

---
+++ Scripts/Backup.ps1
@@ -0,0 +1,6 @@
+cmd .\ScoopBackup.bat
+git add --all
+$date = Get-Date
+$date_string = "{0:yyyy/MM/dd hh:mm:ss}" -f $date
+git commit -m "Backup for $date_string"
+git push origin main
---
+++ Scripts/ScoopRestore.bat
@@ -0,0 +1,1 @@
+scoop import Config/scoopfile.json
