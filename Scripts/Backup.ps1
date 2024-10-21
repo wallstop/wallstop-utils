131209d7ae3bfea9259228d3795036e4e2d96494
wallstop <wallstop@wallstopstudios.com>
2024-10-20 17:24:44 -0700

    Backup for 2024/10/20 05:24:44

--- Scripts/Backup.ps1
+++ Scripts/Backup.ps1
@@ -1,6 +1,6 @@
 $baseDirectory = [IO.Path]::GetDirectoryName((Split-Path -Path $MyInvocation.MyCommand.Definition))
 Push-Location "$baseDirectory/Scripts/"
-./Utils/FormatPowershellScrips.ps1
+./Utils/FormatPowershellScripts.ps1
 ./WindowsTerminal/WindowsTerminalBackup.ps1
 ./Scoop/ScoopUpdate.ps1
 ./Scoop/ScoopBackup.ps1
