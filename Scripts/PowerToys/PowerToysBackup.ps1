1ebe9a1803aab471f4ad18eb901ae62a2b6a8006
wallstop <wallstop@wallstopstudios.com>
2024-10-21 20:28:41 -0700

    Backup for 2024/10/21 08:28:41

---
+++ Scripts/PowerToys/PowerToysBackup.ps1
@@ -0,0 +1,24 @@
+$baseDirectory = [IO.Path]::GetDirectoryName((Split-Path -Path $MyInvocation.MyCommand.Definition))
+Push-Location $baseDirectory
+try {
+  $backupFolder = "$baseDirectory\..\Config\PowerToys"
+
+  if (-not (Test-Path -Path $backupFolder)) {
+    New-Item -Path $backupFolder -ItemType Directory
+  }
+  else {
+    Remove-Item -Path "$backupFolder\*" -Recurse -Force
+  }
+
+  $sourcePath = "$env:LOCALAPPDATA\Microsoft\PowerToys"
+  if (-not (Test-Path -Path $sourcePath)) {
+    Write-Warning "Failed to detect PowerToys config directory at '$sourcePath'."
+    exit 1
+  }
+
+  Copy-Item -Path $sourcePath\*.json -Destination $backupFolder -Recurse -Force
+  Write-Host "PowerToys configuration settings have been backed up to $backupFolder."
+}
+finally {
+  Pop-Location
+}
