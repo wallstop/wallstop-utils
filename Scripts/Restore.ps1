9997e3e9100ce5d4e62aaa586596e391996d68ed
wallstop <wallstop@wallstopstudios.com>
2024-10-24 16:45:31 -0700

    Backup for 2024/10/24 04:45:30

--- Scripts/Restore.ps1
+++ Scripts/Restore.ps1
@@ -2,10 +2,11 @@ $baseDirectory = [IO.Path]::GetDirectoryName((Split-Path -Path $MyInvocation.MyC
 Push-Location "$baseDirectory/Scripts/"
 try {
   ./Scoop/ScoopRestore.ps1
-  ./WindowsTerminal/WindowsTerminalRestore.ps1
   ./Powershell/PowerShellRestore.ps1
+  ./PowerToys/PowerToysRestore.ps1
   ./Config/ConfigRestore.ps1
   ./Komorebi/KomorebRestore.ps1
+  ./WindowsTerminal/WindowsTerminalRestore.ps1
 }
 finally {
   Pop-Location
