4da8c1256fcad91212afaeccfe392dffad7bf687
wallstop <wallstop@wallstopstudios.com>
2024-10-28 11:21:20 -0700

    Backup for 2024/10/28 11:21:20

--- Scripts/Komorebi/KomorebiBackup.ps1
+++ Scripts/Komorebi/KomorebiBackup.ps1
@@ -2,10 +2,12 @@ $rootDirectory = [IO.Path]::GetDirectoryName((Split-Path -Path $MyInvocation.MyC
 $rootDirectory = "$rootDirectory\.."
 Push-Location $rootDirectory
 try {
+  $applicationYaml = "$env:USERPROFILE\applications.yaml"
   $komorebiConfig = "$env:USERPROFILE\komorebi.json"
   $komorebiBarConfig = "$env:USERPROFILE\komorebi.bar.json"
   Copy-Item -Path $komorebiConfig -Destination "$rootDirectory\Config\Komorebi\komorebi.json" -Force
   Copy-Item -Path $komorebiBarConfig -Destination "$rootDirectory\Config\Komorebi\komorebi.bar.json" -Force
+  Copy-Item -Path $applicationYaml -Destination "$rootDirectory\Config\Komorebi\applications.yaml" -Force
   Write-Host "Successfully backed up Komorebi" -ForegroundColor Green
 }
 finally {
