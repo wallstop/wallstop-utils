3137baecc9ed08b31078d74254ccbdb72a2fec59
wallstop <wallstop@wallstopstudios.com>
2024-10-28 14:44:45 -0700

    Backup for 2024/10/28 02:44:45

--- Scripts/Backup.ps1
+++ Scripts/Backup.ps1
@@ -1,17 +1,21 @@
 $baseDirectory = [IO.Path]::GetDirectoryName((Split-Path -Path $MyInvocation.MyCommand.Definition))
 Push-Location "$baseDirectory/Scripts/"
-./Config/ConfigBackup.ps1
-./Utils/FormatPowershellScripts.ps1
-./WindowsTerminal/WindowsTerminalBackup.ps1
-./Powershell/PowershellBackup.ps1
-./Scoop/ScoopUpdate.ps1
-./Scoop/ScoopBackup.ps1
-./Komorebi/KomorebiBackup.ps1
-./PowerToys/PowerToysBackup.ps1
-$date = Get-Date
-$dateString = "{0:yyyy/MM/dd hh:mm:ss}" -f $date
-git add --all
-git commit -m "Backup for $dateString"
-git pull origin main
-git push origin main
-Pop-Location
+try {
+  ./Config/ConfigBackup.ps1
+  ./Utils/FormatPowershellScripts.ps1
+  ./WindowsTerminal/WindowsTerminalBackup.ps1
+  ./Powershell/PowershellBackup.ps1
+  ./Scoop/ScoopUpdate.ps1
+  ./Scoop/ScoopBackup.ps1
+  ./Komorebi/KomorebiBackup.ps1
+  ./PowerToys/PowerToysBackup.ps1
+  ./WinGet/WinGetUpdate.ps1
+  $date = Get-Date
+  $dateString = "{0:yyyy/MM/dd hh:mm:ss}" -f $date
+  git add --all
+  git commit -m "Backup for $dateString"
+  git pull origin main
+  git push origin main
+} finally {
+  Pop-Location
+}
