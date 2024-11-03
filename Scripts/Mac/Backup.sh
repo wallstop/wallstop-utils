df9ad002b8254511ff16328bd8db66bb3ac26f2d
Eli Pinkerton <wallstop@wallstopstudios.com>
2024-11-02 20:40:10 -0700

    Add mac backups

---
+++ Scripts/Mac/Backup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Directory to store the backups
+BACKUP_DIR="../../Config/Mac"
+mkdir -p "$BACKUP_DIR"
+
+dotfile_count=0
+applescript_count=0
+
+# Find all dotfiles in the home directory and copy them to the backup directory
+for file in $HOME/.*; do
+    # Skip the special directories '.' and '..'
+    # Skip special directories and any subdirectories
+    if [[ "$file" == "$HOME/." || "$file" == "$HOME/.." || -d "$file" ]]; then
+        continue
+    fi
+
+    # Copy each dotfile to the backup directory
+    cp -p "$file" "$BACKUP_DIR/"
+    ((dotfile_count++))
+done
+
+# Backup AppleScript files in the home directory
+for file in $HOME/*.{scpt,applescript}; do
+    # Check if the file exists to handle the case where no AppleScript files are present
+    if [[ -e "$file" ]]; then
+        cp -p "$file" "$BACKUP_DIR/"
+        ((applescript_count++))
+    fi
+done
+
+echo "Backup completed. $dotfile_count dotfiles and $applescript_count AppleScript files were copied to $BACKUP_DIR"
+
