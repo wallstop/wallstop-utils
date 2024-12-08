e65d6ce81954a93da97e17ec0920a55a8c08b6b4
Eli Pinkerton <wallstop@wallstopstudios.com>
2024-12-08 11:17:13 -0800

    More noodling

--- Scripts/Utils/LaunchUnity.bat
+++ Scripts/Utils/LaunchUnity.bat
@@ -2,38 +2,26 @@
 echo Starting Unity launcher script...
 
 set "EDITOR_RUNNING="
-set "EDITOR_NAME="
-for /f "tokens=*" %%i in ('tasklist ^| find "Unity.exe"') do (
-    set EDITOR_RUNNING=1
-    set "EDITOR_NAME=%%i"
-)
+for /f %%i in ('tasklist ^| find "Unity.exe"') do set EDITOR_RUNNING=1
 
 if defined EDITOR_RUNNING (
-    echo Unity Editor process found: %EDITOR_NAME%
+    echo Unity Editor process found
     echo Attempting to focus Unity Editor window...
-    start "" wscript //nologo "%~dp0Focus.vbs" "%EDITOR_NAME%"
-    echo Focus attempt completed
+    powershell -window hidden -command "$signature='[DllImport(\"user32.dll\")] public static extern bool SetForegroundWindow(IntPtr hWnd);'; Add-Type -MemberDefinition $signature -Name Win32ShowWindow -Namespace Win32Functions; Get-Process Unity | ForEach-Object { [Win32Functions.Win32ShowWindow]::SetForegroundWindow($_.MainWindowHandle); }"
 ) else (
     echo Unity Editor process not found
     echo Checking for Unity Hub...
     
     set "HUB_RUNNING="
-    set "HUB_NAME="
-    for /f "tokens=*" %%i in ('tasklist ^| find "Unity Hub.exe"') do (
-        set HUB_RUNNING=1U
-        set "HUB_NAME=%%i"
-    )
+    for /f %%i in ('tasklist ^| find "Unity Hub.exe"') do set HUB_RUNNING=1
     
     if defined HUB_RUNNING (
-        echo Unity Hub process found: %HUB_NAME%
+        echo Unity Hub process found
         echo Attempting to focus Unity Hub...
-        start "" wscript //nologo "%~dp0Focus.vbs" "%HUB_NAME%"
-        echo Focus attempt completed
+        powershell -window hidden -command "$signature='[DllImport(\"user32.dll\")] public static extern bool SetForegroundWindow(IntPtr hWnd);'; Add-Type -MemberDefinition $signature -Name Win32ShowWindow -Namespace Win32Functions; Get-Process 'Unity Hub' | ForEach-Object { [Win32Functions.Win32ShowWindow]::SetForegroundWindow($_.MainWindowHandle); }"
     ) else (
         echo No Unity processes found
         echo Launching Unity Editor...
         start "" "C:\Program Files\Unity\Hub\Editor\6000.0.23f1\Editor\Unity.exe"
-        echo Launch command sent
     )
 )
-echo Script complete
